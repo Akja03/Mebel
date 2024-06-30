72c4618a24642e3f612c1b63e3321613736f1bf1
Akja03 <akjakakalyyewa74@gmail.com>
2024-06-30 10:56:56 -0700

    1.0.0

--- app/Models/Attribute.php
+++ app/Models/Attribute.php
@@ -7,31 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attribute extends Model
 {
-    protected $fillable = [
-        'name',
-        'description',
-        'price',
-        'category_id'
-    ];
-
-    public function category()
-    {
-        return $this->belongsTo(Category::class);
-    }
-
-    public function orderItem()
-    {
-        return $this->hasMany(OrderItem::class);
-    }
-
-    public function attribute()
-    {
-        return $this->belongsToMany(Attribute::class, 'product_attributes');
-    }
-
-    public function reviews()
-    {
-        return $this->hasMany(Review::class);
-    }
 
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,5 +7,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'category_id'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orderItem()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function attribute()
+    {
+        return $this->belongsToMany(Attribute::class, 'product_attributes');
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
