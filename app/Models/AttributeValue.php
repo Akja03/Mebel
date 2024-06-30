4dc8d7eeb3f2a63763a3fbee7317e112d6fbfff5
Akja03 <akjakakalyyewa74@gmail.com>
2024-06-30 11:09:18 -0700

    1.3.0

--- app/Models/AttributeValue.php
+++ app/Models/AttributeValue.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class AttributeValue extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'value',
+        'attribute_id'
+    ];
+
+    public function attribute()
+    {
+        return $this->belongsTo(Attribute::class);
+    }
 }
